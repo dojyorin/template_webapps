3ccbebdd8020907b0b248aeb0194d21aa3e6f0da
dojyorin <ota_droid@live.jp>
2023-07-12 15:02:07 +0900

    WIP.

--- src/gpgpu.ts
+++ src/gpgpu.ts
@@ -1,18 +1,19 @@
-interface NativeNumber{
-    i8: Int8Array;
-    u8: Uint8Array;
-    i16: Int16Array;
-    u16: Uint16Array;
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext"/>
+/// <reference lib="dom"/>
+
+// @deno-types="https://esm.sh/@webgpu/types@0.1.34/dist/index.d.ts"
+
+interface GPGPUArray{
     i32: Int32Array;
     u32: Uint32Array;
     f32: Float32Array;
-    f64: Float64Array;
 }
 
 interface GPGPUBind{
     group: number;
     binding: number;
-    type: keyof NativeNumber;
+    type: keyof GPGPUArray;
     target: GPUBuffer;
 }
 
@@ -20,78 +21,63 @@ interface GPGPUBindView extends Omit<GPGPUBind, "target">{
     size: number;
 }
 
+interface GPGPUVec3{
+    x: number;
+    y: number;
+    z: number;
+}
+
 export class GPGPU{
     #device?:GPUDevice;
-    #command?:GPUCommandBuffer;
+    #program?:GPUCommandBuffer;
     #binds:GPGPUBind[];
 
     constructor(){
         this.#device = undefined;
-        this.#command = undefined;
+        this.#program = undefined;
         this.#binds = [];
     }
 
-    static #ta<T extends keyof NativeNumber>(type:T){
+    static #arrayCon<T extends keyof GPGPUArray>(type:T){
         switch(type){
-            case "i8": return Int8Array;
-            case "u8": return Uint8Array;
-            case "i16": return Int16Array;
-            case "u16": return Uint16Array;
             case "i32": return Int32Array;
             case "u32": return Uint32Array;
             case "f32": return Float32Array;
-            case "f64": return Float64Array;
+            default: throw new Error();
         }
     }
 
-    #findBind(g:number, b:number){
-        return this.#binds.find(({group, binding}) => group === g && binding === b);
-    }
-
-    get binds(){
-        return this.#binds.map(v => ({
-            group: v.group,
-            binding: v.binding,
-            type: v.type,
-            size: v.target.size
-        }));
-    }
-
-    async read(group:number, binding:number){
-        const bind = this.#findBind(group, binding);
+    #findBind(group:number, binding:number){
+        const bind = this.#binds.find(v => v.group === group && v.binding === binding);
 
         if(!bind){
             throw new ReferenceError();
         }
 
-        const stage = this.#device.createBuffer({
-            size: bind.target.size,
-            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-            mappedAtCreation: false
-        });
-
-        const command = this.#device.createCommandEncoder();
-        command.copyBufferToBuffer(bind.target, 0, stage, 0, stage.size);
-        this.#device.queue.submit([command.finish()]);
-        await stage.mapAsync(GPUMapMode.READ);
-
-        const TA = GPGPU.#ta(bind.type);
-        return new TA(stage.getMappedRange());
+        return bind;
     }
 
-    write<T extends NativeNumber[keyof NativeNumber]>(group:number, binding:number, data:T){
-        const bind = this.#findBind(group, binding);
+    get binds(){
+        const views:GPGPUBindView[] = [];
 
-        const TA = GPGPU.#ta(bind.type);
-        if(!bind || !(data instanceof TA) || data.byteLength !== bind.target.size){
-            throw new ReferenceError();
+        for(const {group, binding, target, type} of this.#binds){
+            views.push({
+                group: group,
+                binding: binding,
+                type: type,
+                size: target.size
+            })
         }
 
-        this.#device.queue.writeBuffer(bind.target, 0, data, 0);
+        return views;
     }
 
-    async setup(){
-        if(!navigator.gpu){
+    async begin(){
+        if(this.#device){
+            return;
+        }
+
+        if(!globalThis?.navigator?.gpu){
             throw new ReferenceError();
         }
 
@@ -110,7 +96,11 @@ export class GPGPU{
         }
     }
 
-    async pipeline(wgsl:string){
+    async createCommand(wgsl:string, count:GPGPUVec3){
+        if(!this.#device){
+            throw new ReferenceError();
+        }
+
         const command = this.#device.createCommandEncoder();
         const pass = command.beginComputePass();
 
@@ -126,43 +116,97 @@ export class GPGPU{
 
         pass.setPipeline(pipeline);
 
-        for(const variable of wgsl.match(/@ {0,}(group|binding)[^;]+/gs).map(v => v.replace(/\n/g, " "))){
+        for(const variable of wgsl.match(/@ {0,}(group|binding)[^;]+/gs)?.map(v => v.replace(/\r|\n|\t/g, " ")) ?? []){
             const [, g] = variable.match(/@ {0,}group {0,}\( {0,}(\d+)/) ?? [];
             const [, b] = variable.match(/@ {0,}binding {0,}\( {0,}(\d+)/) ?? [];
-            const [, t, s] = variable.match(/array {0,}< {0,}(i8|u8|i16|u16|i32|u32|f32|f64) {0,}, {0,}(\d+)/) ?? [];
+            const [, t, s] = variable.match(/array {0,}< {0,}(i32|u32|f32) {0,}, {0,}(\d+)/) ?? [];
 
             this.#binds.push({
                 group: Number(g),
                 binding: Number(b),
-                type: t,
+                type: <keyof GPGPUArray>t,
                 target: this.#device.createBuffer({
-                    size: GPGPU.#ta(t).BYTES_PER_ELEMENT * Number(s),
+                    size: GPGPU.#arrayCon(<keyof GPGPUArray>t).BYTES_PER_ELEMENT * Number(s),
                     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
                 })
             });
         }
 
         for(const group of new Set(this.#binds.map(({group}) => group))){
-            pass.setBindGroup(group, this.#device.createBindGroup({
-                layout: pipeline.getBindGroupLayout(group),
-                entries: this.#binds.filter(v => v.group === group).map(({binding, target}) => ({
+            const entries:GPUBindGroupEntry[] = [];
+
+            for(const {binding, target} of this.#binds){
+                entries.push({
                     binding: binding,
                     resource: {
                         buffer: target
                     }
-                }))
-            }));
+                });
+            }
+
+            const bg = this.#device.createBindGroup({
+                layout: pipeline.getBindGroupLayout(group),
+                entries: entries
+            });
+
+            pass.setBindGroup(group, bg);
         }
 
         const [, x, y, z] = wgsl.match(/@ {0,}workgroup_size {0,}\( {0,}(\d+) {0,}, {0,}(\d+) {0,}, {0,}(\d+)/) ?? [];
 
-        pass.dispatchWorkgroups(Number(x), Number(y ?? "1"), Number(z ?? "1"));
+        pass.dispatchWorkgroups(Number(x) / count.x, Number(y) / count.y, Number(z) / count.z);
         pass.end();
 
-        this.#command = command.finish();
+        this.#program = command.finish();
     }
 
     execute(){
-        this.#device.queue.submit([this.#command]);
+        if(!this.#device || !this.#program){
+            throw new ReferenceError();
+        }
+
+        this.#device.queue.submit([this.#program]);
+    }
+
+    async read<T extends keyof GPGPUArray>(group:number, binding:number, type:T){
+        if(!this.#device){
+            throw new ReferenceError();
+        }
+
+        const bind = this.#findBind(group, binding);
+
+        if(bind.type !== type){
+            throw new ReferenceError();
+        }
+
+        const stage = this.#device.createBuffer({
+            size: bind.target.size,
+            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+            mappedAtCreation: false
+        });
+
+        const command = this.#device.createCommandEncoder();
+        command.copyBufferToBuffer(bind.target, 0, stage, 0, stage.size);
+
+        this.#device.queue.submit([command.finish()]);
+
+        await stage.mapAsync(GPUMapMode.READ);
+
+        return <GPGPUArray[T]>new (GPGPU.#arrayCon(bind.type))(stage.getMappedRange());
+    }
+
+    write<T extends GPGPUArray[keyof GPGPUArray]>(group:number, binding:number, data:T){
+        if(!this.#device){
+            throw new ReferenceError();
+        }
+
+        const bind = this.#findBind(group, binding);
+
+        const TA = GPGPU.#arrayCon(bind.type);
+        if(!(data instanceof TA)){
+            throw new ReferenceError();
+        }
+
+        this.#device.queue.writeBuffer(bind.target, 0, data, 0);
     }
 }
